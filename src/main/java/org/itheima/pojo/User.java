65e146af90089f6cdefad172aa1b52ec293e8078
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-04-27 20:16:50 +0800

    add method to update validated userInfo

--- src/main/java/org/itheima/pojo/User.java
+++ src/main/java/org/itheima/pojo/User.java
@@ -3,17 +3,26 @@ package org.itheima.pojo;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 // lombok 在编译阶段，为实体类自动生成getter/setter/toString等方法
 @Data
 public class User {
+    @NotNull
     private Integer id;//主键ID
     private String username;//用户名
     @JsonIgnore
     private String password;//密码
+    @NotEmpty
+    @Pattern(regexp = "^\\S{1,10}$")
     private String nickname;//昵称
+    @NotEmpty
+    @Email
     private String email;//邮箱
     private String userPic;//用户头像地址
     private LocalDateTime createTime;//创建时间

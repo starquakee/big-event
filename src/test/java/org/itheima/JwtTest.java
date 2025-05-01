ad57ab57321d6808cfb031114cc6af55d4db8ff7
STAR-QUAKE <99738745+starquakee@users.noreply.github.com>
2025-05-01 23:18:46 +0800

    Project Deployment

--- src/test/java/org/itheima/JwtTest.java
+++ src/test/java/org/itheima/JwtTest.java
@@ -1,38 +1,38 @@
-package org.itheima;
-
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.Claim;
-import com.auth0.jwt.interfaces.DecodedJWT;
-import org.junit.jupiter.api.Test;
-
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
-public class JwtTest {
-    @Test
-    public void testGen() {
-        Map<String, Object> claims = new HashMap<>();
-        claims.put("id", "1");
-        claims.put("username", "zhangsan");
-        // 生成jwt的代码
-        String token = JWT.create()
-                .withClaim("user", claims)// 设置载荷
-                .withExpiresAt(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 12))// 设置过期时间
-                .sign(Algorithm.HMAC256( "itheima"));// 设置密钥;
-        System.out.println(token);
-    }
-
-    @Test
-    public void testParse() {
-        String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9" +
-                ".eyJ1c2VyIjp7ImlkIjoiMSIsInVzZXJuYW1lIjoiemhhbmdzYW4ifSwiZXhwIjoxNzQ1NzIwODYzfQ" +
-                "._l5JV-_XARafrU7vw_eH1YHeqAWo5q88VBADHEe7Fmo";
-        JWTVerifier verifier = JWT.require(Algorithm.HMAC256("itheima")).build();
-        DecodedJWT jwt = verifier.verify(token);
-        Map<String, Claim> claims = jwt.getClaims();
-        System.out.println(claims.get("user"));
-    }
-}
+//package org.itheima;
+//
+//import com.auth0.jwt.JWT;
+//import com.auth0.jwt.JWTVerifier;
+//import com.auth0.jwt.algorithms.Algorithm;
+//import com.auth0.jwt.interfaces.Claim;
+//import com.auth0.jwt.interfaces.DecodedJWT;
+//import org.junit.jupiter.api.Test;
+//
+//import java.util.Date;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//public class JwtTest {
+//    @Test
+//    public void testGen() {
+//        Map<String, Object> claims = new HashMap<>();
+//        claims.put("id", "1");
+//        claims.put("username", "zhangsan");
+//        // 生成jwt的代码
+//        String token = JWT.create()
+//                .withClaim("user", claims)// 设置载荷
+//                .withExpiresAt(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 12))// 设置过期时间
+//                .sign(Algorithm.HMAC256( "itheima"));// 设置密钥;
+//        System.out.println(token);
+//    }
+//
+//    @Test
+//    public void testParse() {
+//        String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9" +
+//                ".eyJ1c2VyIjp7ImlkIjoiMSIsInVzZXJuYW1lIjoiemhhbmdzYW4ifSwiZXhwIjoxNzQ1NzIwODYzfQ" +
+//                "._l5JV-_XARafrU7vw_eH1YHeqAWo5q88VBADHEe7Fmo";
+//        JWTVerifier verifier = JWT.require(Algorithm.HMAC256("itheima")).build();
+//        DecodedJWT jwt = verifier.verify(token);
+//        Map<String, Claim> claims = jwt.getClaims();
+//        System.out.println(claims.get("user"));
+//    }
+//}
--- src/test/java/org/itheima/RedisTest.java
+++ src/test/java/org/itheima/RedisTest.java
@@ -1,26 +1,26 @@
-package org.itheima;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
-
-@SpringBootTest
-public class RedisTest {
-    @Autowired
-    private StringRedisTemplate stringRedisTemplate;
-    @Test
-    public void testSet() {
-        //向redis中存储一个键值对
-        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
-        ops.set("name", "zhangsan");
-    }
-
-    @Test
-    public void testGet() {
-        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
-        String name = ops.get("username");
-        System.out.println(name);
-    }
-}
+//package org.itheima;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.data.redis.core.StringRedisTemplate;
+//import org.springframework.data.redis.core.ValueOperations;
+//
+//@SpringBootTest
+//public class RedisTest {
+//    @Autowired
+//    private StringRedisTemplate stringRedisTemplate;
+//    @Test
+//    public void testSet() {
+//        //向redis中存储一个键值对
+//        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+//        ops.set("name", "zhangsan");
+//    }
+//
+//    @Test
+//    public void testGet() {
+//        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+//        String name = ops.get("username");
+//        System.out.println(name);
+//    }
+//}
